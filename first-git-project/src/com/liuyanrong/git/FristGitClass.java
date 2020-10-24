abc6aec5607220635aefa979ba1179a8ce45b22b
liuyanrong <635451543@qq.com>
2020-10-24 15:21:24 +0800

    把本地工程添加到git

---
+++ first-git-project/src/com/liuyanrong/git/FristGitClass.java
@@ -0,0 +1,14 @@
+package com.liuyanrong.git;
+
+/**
+ * Created by 86187 on 2020/10/24.
+ */
+public class FristGitClass {
+    public static void main(String[] args) {
+        System.out.println("Frist----git---Project---1");
+        System.out.println("Frist----git---Project---2");
+        System.out.println("Frist----git---Project---3");
+        System.out.println("Frist----git---Project---4");
+        System.out.println("Frist----git---Project---5");
+    }
+}
